4760bce0d0fbbed45e87d32ce21075c8c961e56a
Abhimanyu Bundela <abundela12345@gmail.com>
2024-10-07 00:10:39 +0530

    SlideNoAnim
    Physics based Slide Mechanism

--- Source/ParkourSystem/Private/PS_CharacterMovementComponent.cpp
+++ Source/ParkourSystem/Private/PS_CharacterMovementComponent.cpp
@@ -3,10 +3,19 @@
 
 #include "PS_CharacterMovementComponent.h"
 #include "GameFramework/Character.h"
+#include "ParkourSystemCharacter.h"
+#include "Components/CapsuleComponent.h"
 
 UPS_CharacterMovementComponent::UPS_CharacterMovementComponent()
 {
 	NavAgentProps.bCanCrouch = true;
+}
+
+void UPS_CharacterMovementComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	PS_CharacterOwner = Cast<AParkourSystemCharacter>(GetOwner());
 }
 
 FNetworkPredictionData_Client* UPS_CharacterMovementComponent::GetPredictionData_Client() const
@@ -47,6 +56,146 @@ void UPS_CharacterMovementComponent::OnMovementUpdated(float DeltaSeconds, const
 			MaxWalkSpeed = Walk_MaxWalkSpeed;
 		}
 	}
+
+	Safe_bPrevWantsToCrouch = bWantsToCrouch;
+}
+
+bool UPS_CharacterMovementComponent::IsMovingOnGround() const
+{
+	return Super::IsMovingOnGround() || IsCustomMovementMode(CusMOVE_Slide);
+}
+
+bool UPS_CharacterMovementComponent::CanCrouchInCurrentState() const
+{
+	return Super::CanCrouchInCurrentState() && IsMovingOnGround();
+}
+
+void UPS_CharacterMovementComponent::UpdateCharacterStateBeforeMovement(float DeltaSeconds)
+{
+	if (MovementMode == MOVE_Walking && !bWantsToCrouch && Safe_bPrevWantsToCrouch)
+	{
+		FHitResult PotentialSlideSurface;
+		if (Velocity.SizeSquared() > pow(Slide_MinSpeed, 2) && GetSlideSurface(PotentialSlideSurface))
+		{
+			EnterSlide();
+		}
+	}
+
+	if (IsCustomMovementMode(CusMOVE_Slide) && !bWantsToCrouch)
+	{
+		ExitSlide();
+	}
+
+	Super::UpdateCharacterStateBeforeMovement(DeltaSeconds);
+}
+
+void UPS_CharacterMovementComponent::PhysCustom(float DeltaTime, int32 Iterations)
+{
+	Super::PhysCustom(DeltaTime, Iterations);
+
+	switch (CustomMovementMode)
+	{
+	case CusMOVE_Slide:
+		PhysSlide(DeltaTime, Iterations);
+		break;
+	default:
+		UE_LOG(LogTemp, Fatal, TEXT("Invalid Movement Mode"))
+	}
+}
+
+void UPS_CharacterMovementComponent::EnterSlide()
+{
+	bWantsToCrouch = true;
+	Velocity += Velocity.GetSafeNormal2D() * Slide_EnterImpulse;
+	SetMovementMode(MOVE_Custom, CusMOVE_Slide);
+}
+
+void UPS_CharacterMovementComponent::ExitSlide()
+{
+	bWantsToCrouch = false;
+
+	FQuat NewRotation = FRotationMatrix::MakeFromXZ(UpdatedComponent->GetForwardVector().GetSafeNormal2D(), FVector::UpVector).ToQuat();
+	FHitResult Hit;
+	SafeMoveUpdatedComponent(FVector::ZeroVector, NewRotation, true, Hit);
+	SetMovementMode(MOVE_Walking);
+}
+
+void UPS_CharacterMovementComponent::PhysSlide(float DeltaTime, int32 Iterations)
+{
+	if (DeltaTime < MIN_TICK_TIME)
+	{
+		return;
+	}
+
+	RestorePreAdditiveRootMotionVelocity();
+
+	FHitResult SurfaceHit;
+	if (!GetSlideSurface(SurfaceHit) || Velocity.SizeSquared() < pow(Slide_MinSpeed, 2))
+	{
+		ExitSlide();
+		StartNewPhysics(DeltaTime, Iterations);
+		return;
+	}
+
+	Velocity += Slide_GravityForce * FVector::DownVector * DeltaTime;
+
+	if (FMath::Abs(FVector::DotProduct(Acceleration.GetSafeNormal(), UpdatedComponent->GetRightVector())) > 0.5f)
+	{
+		Acceleration = Acceleration.ProjectOnTo(UpdatedComponent->GetRightVector());
+	}
+	else
+	{
+		Acceleration = FVector::ZeroVector;
+	}
+
+	if (!HasAnimRootMotion() && !CurrentRootMotion.HasOverrideVelocity())
+	{
+		CalcVelocity(DeltaTime, Slide_Friction, true, GetMaxBrakingDeceleration());
+	}
+	ApplyRootMotionToVelocity(DeltaTime);
+
+	Iterations++;
+	bJustTeleported = false;
+
+	FVector OldLocation = UpdatedComponent->GetComponentLocation();
+	FQuat OldRotation = UpdatedComponent->GetComponentRotation().Quaternion();
+	FHitResult Hit(1.f);
+	FVector Adjusted = Velocity * DeltaTime;
+	FVector VelPlaneDir = FVector::VectorPlaneProject(Velocity, SurfaceHit.Normal).GetSafeNormal();
+	FQuat NewRotation = FRotationMatrix::MakeFromXZ(VelPlaneDir, SurfaceHit.Normal).ToQuat();
+	SafeMoveUpdatedComponent(Adjusted, NewRotation, true, Hit);
+
+	if (Hit.Time < 1.f)
+	{
+		HandleImpact(Hit, DeltaTime, Adjusted);
+		SlideAlongSurface(Adjusted, (1.f - Hit.Time), Hit.Normal, Hit, true);
+	}
+
+	FHitResult NewSurfaceHit;
+	if (!GetSlideSurface(NewSurfaceHit) || Velocity.SizeSquared() < pow(Slide_MinSpeed, 2))
+	{
+		ExitSlide();
+	}
+
+	if (!bJustTeleported && !HasAnimRootMotion() && !CurrentRootMotion.HasOverrideVelocity())
+	{
+		Velocity = (UpdatedComponent->GetComponentLocation() - OldLocation) / DeltaTime;
+	}
+}
+
+bool UPS_CharacterMovementComponent::GetSlideSurface(FHitResult& Hit) const
+{
+	FVector Start = UpdatedComponent->GetComponentLocation();
+	FVector End = Start + CharacterOwner->GetCapsuleComponent()->GetScaledCapsuleHalfHeight() * 2.f * FVector::DownVector;
+	FName ProfileName = TEXT("BlockAll");
+
+	FCollisionQueryParams Params;
+	TArray<AActor*> OwnerChildren;
+	PS_CharacterOwner->GetAllChildActors(OwnerChildren);
+	Params.AddIgnoredActors(OwnerChildren);
+	Params.AddIgnoredActor(PS_CharacterOwner);
+
+	return GetWorld()->LineTraceSingleByProfile(Hit, Start, End, ProfileName, Params);
 }
 
 void UPS_CharacterMovementComponent::SprintPressed()
@@ -62,6 +211,11 @@ void UPS_CharacterMovementComponent::SprintReleased()
 void UPS_CharacterMovementComponent::CrouchPressed()
 {
 	bWantsToCrouch = ~bWantsToCrouch;
+}
+
+bool UPS_CharacterMovementComponent::IsCustomMovementMode(ECustomMovementMode InCustomMovementMode) const
+{
+	return MovementMode == MOVE_Custom && CustomMovementMode == InCustomMovementMode;
 }
 
 bool UPS_CharacterMovementComponent::FSavedMove_PS::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const
@@ -99,6 +253,7 @@ void UPS_CharacterMovementComponent::FSavedMove_PS::SetMoveFor(ACharacter* C, fl
 	UPS_CharacterMovementComponent* CharacterMovement = Cast<UPS_CharacterMovementComponent>(C->GetCharacterMovement());
 
 	Saved_bWantsToSprint = CharacterMovement->Safe_bWantsToSprint;
+	Saved_bPrevWantsToCrouch = CharacterMovement->Safe_bPrevWantsToCrouch;
 }
 
 void UPS_CharacterMovementComponent::FSavedMove_PS::PrepMoveFor(ACharacter* C)
@@ -108,6 +263,7 @@ void UPS_CharacterMovementComponent::FSavedMove_PS::PrepMoveFor(ACharacter* C)
 	UPS_CharacterMovementComponent* CharacterMovement = Cast<UPS_CharacterMovementComponent>(C->GetCharacterMovement());
 
 	CharacterMovement->Safe_bWantsToSprint = Saved_bWantsToSprint;
+	CharacterMovement->Safe_bPrevWantsToCrouch = Saved_bPrevWantsToCrouch;
 }
 
 UPS_CharacterMovementComponent::FNetworkPredictionData_Client_PS::FNetworkPredictionData_Client_PS(const UCharacterMovementComponent& ClientMovement)
--- Source/ParkourSystem/Public/PS_CharacterMovementComponent.h
+++ Source/ParkourSystem/Public/PS_CharacterMovementComponent.h
@@ -6,6 +6,15 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "PS_CharacterMovementComponent.generated.h"
 
+
+UENUM(BlueprintType)
+enum ECustomMovementMode
+{
+	CusMOVE_None UMETA(Hidden),
+	CusMOVE_Slide UMETA(DisplayName = "Slide"),
+	CusMOVE_MAX UMETA(Hidden),
+};
+
 /**
  * 
  */
@@ -18,7 +27,10 @@ class PARKOURSYSTEM_API UPS_CharacterMovementComponent : public UCharacterMoveme
 	{
 		typedef FSavedMove_Character Super;
 
+		//Flag
 		uint8 Saved_bWantsToSprint : 1;
+
+		uint8 Saved_bPrevWantsToCrouch : 1;
 
 		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
 		virtual void Clear() override;
@@ -39,24 +51,52 @@ class PARKOURSYSTEM_API UPS_CharacterMovementComponent : public UCharacterMoveme
 
 	UPROPERTY(EditDefaultsOnly) float Sprint_MaxWalkSpeed;
 	UPROPERTY(EditDefaultsOnly) float Walk_MaxWalkSpeed;
+
+
+	UPROPERTY(EditDefaultsOnly) float Slide_MinSpeed = 350.f;
+	UPROPERTY(EditDefaultsOnly) float Slide_EnterImpulse = 500.f;
+	UPROPERTY(EditDefaultsOnly) float Slide_GravityForce = 5000.f;
+	UPROPERTY(EditDefaultsOnly) float Slide_Friction = 1.3f;
+
 	
+	UPROPERTY(Transient) class AParkourSystemCharacter* PS_CharacterOwner;
+
 	bool Safe_bWantsToSprint;
+	bool Safe_bPrevWantsToCrouch;
 
 
 public:
 	UPS_CharacterMovementComponent();
 
+protected:
+	virtual void InitializeComponent() override;
+
 public:
 	virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+	virtual bool IsMovingOnGround() const override;
+	virtual bool CanCrouchInCurrentState() const override;
 
 protected:
 	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
 
 	virtual void OnMovementUpdated(float DeltaSeconds, const FVector& OldLocation, const FVector& OldVeclocity) override;
 
+	virtual void UpdateCharacterStateBeforeMovement(float DeltaSeconds) override;
+
+	virtual void PhysCustom(float DeltaTime, int32 Iterations) override;
+
+private:
+	void EnterSlide();
+	void ExitSlide();
+	void PhysSlide(float DeltaTime, int32 Iterations);
+	bool GetSlideSurface(FHitResult& Hit) const;
+
 public:
 	UFUNCTION(BlueprintCallable) void SprintPressed();
 	UFUNCTION(BlueprintCallable) void SprintReleased();
 
 	UFUNCTION(BlueprintCallable) void CrouchPressed();
+
+	UFUNCTION(BlueprintPure) bool IsCustomMovementMode(ECustomMovementMode InCustomMovementMode) const;
 };
